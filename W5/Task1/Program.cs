c0cd8bc1844c4f09464a92fa432c5a15ade0e71f
Amangeldi Yersultan <ereke.amangeldi@gmail.com>
2019-02-22 19:27:59 +0600

    fourth commit

---
+++ W5/Task1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace task1
+ {
+    public class ComplexNumbers//class complex numbers
+    {
+        public double a;//first variable;
+        public double b;//second variable;
+        public void PrintInfo()
+        {
+            Console.WriteLine(a + "+" + b + "i");//this is a formula;
+        }
+        public void PrintFormula()
+        {
+            Console.WriteLine("a + bi");
+        }
+        public ComplexNumbers()//this requires to if u want to serialize;
+        {
+
+        }
+
+      
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            F1();//we call function;
+        }
+        public static void F1()
+        {
+
+
+            ComplexNumbers cn = new ComplexNumbers();//according to the characteristics of class Mark we create  ;
+            cn.PrintInfo();//this is a solution;
+            cn.PrintFormula();//write in console formula
+
+
+            FileStream fs = new FileStream("cn.txt", FileMode.Create, FileAccess.Write);
+            XmlSerializer xs = new XmlSerializer(typeof(ComplexNumbers));
+
+            xs.Serialize(fs, cn);//serializing-it's to save ur condition to txt or xml file to network with others in the internet or e.t.c
+            fs.Close();
+
+        }
+    }
+}
---
+++ W5/Task2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+
+namespace Task2._1
+{
+    public class Mark // create class num;
+    {
+        public int num;
+        public string ltr;
+        public void getLetter() //create function getLetter
+        {
+            if (num <= 100 && num >= 95)
+                ltr += "A+";
+            if (num < 95 && num >= 90)
+                ltr += "A-";
+        }
+        public override string ToString()
+        {
+            return ltr;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            F1();//we call function;
+        }
+        public static void F1()
+        {
+            Mark m = new Mark();// according to the characteristics of class Mark we create  ;
+            m.num = int.Parse(Console.ReadLine());// first chararteristic is num
+            m.getLetter();// we check it and give char;
+            Console.WriteLine(m);// we will print Mark in Console using to string 
+
+            FileStream fs = new FileStream("mark.txt", FileMode.Create, FileAccess.Write);// open file that we will serialize
+            XmlSerializer xs = new XmlSerializer(typeof(Mark));
+
+            xs.Serialize(fs, m);//serializing;
+            fs.Close();
+
+
+        }
+    }
+}
